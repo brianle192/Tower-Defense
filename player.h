8195bf80b660bf787a15fc6afbdb96f3a48068d1
brianle192 <letancuong@gmail.com>
2017-02-21 22:14:13 -0800

    Update player.h

--- player.h
+++ player.h
@@ -1,10 +1,6 @@
 //
 //  player.h
-//  InstallingSFML
-//
-//  Created by Blank Blank on 2015-08-11.
-//  Copyright (c) 2015 Blank Blank. All rights reserved.
-//
+
 
 #ifndef __InstallingSFML__player__
 #define __InstallingSFML__player__
