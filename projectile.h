ef2a0d8798a449b6a55eea05aaa8434d84b9606b
brianle192 <letancuong@gmail.com>
2017-02-21 22:13:41 -0800

    Update projectile.h

--- projectile.h
+++ projectile.h
@@ -1,10 +1,6 @@
 //
 //  projectile.h
-//  InstallingSFML
-//
-//  Created by Blank Blank on 2015-08-11.
-//  Copyright (c) 2015 Blank Blank. All rights reserved.
-//
+
 
 #ifndef __InstallingSFML__projectile__
 #define __InstallingSFML__projectile__
