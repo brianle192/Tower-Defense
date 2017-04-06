ff07987e648ccd59bb9270833c59f17db5f71ae9
brianle192 <letancuong@gmail.com>
2017-04-05 21:23:56 -0700

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -7,7 +7,7 @@ public:
 	//float movementSpeed = 0.5; //slower
 	int movementSpeed = 1;
 	int movementLength = 100; //50 move shorter
-	int attackDamage = 2;
+	int attackDamage = 1;
 	int counterWalking = 0;
 	int direction = 0; // 1 - up, 2 - down, 3 - left, 4 - right
 	int counter = 0;
