8533a966041968a3703ec83b70610d92f555de31
brianle192 <letancuong@gmail.com>
2017-04-14 00:01:38 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -6,15 +6,14 @@ Player::Player(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, 
 {
 	this->speed = speed;
 	row = 0;
-	faceRight = true;
+	face = true;
 
-	body.setSize(sf::Vector2f(45.0f, 45.0f));
-	body.setPosition(360.0f, 240.0f);
+	body.setSize(sf::Vector2f(40.0f, 40.0f));
+	body.setPosition(-360.0f, 240.0f);
 	body.setTexture(texture);
 
 	view.setSize(720, 480);
 	view.setCenter(360.0f, 240.0f);
-	//view.zoom(0.6f);
 }
 
 
@@ -22,17 +21,17 @@ Player::~Player()
 {
 }
 
-
 void Player::Update(float deltaTime)
 {
 	movement.x = 0.0f;
 	movement.y = 0.0f;
 
+	//Gerardo 4/4
+	//Added a +0.5 to movement speed, maybe could be a variable on power up you can move faster
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
 		sound.play();
 		if (faceLeft == true) {
-			std::cout << "moving left" << std::endl;
-			movement.x -= ((speed * deltaTime));		//Gerardo: added + 4 to move in blocks
+			movement.x -= ((speed * deltaTime) + 0.5);		//Gerardo: added + 4 to move in blocks
 			direction = 3;
 			faceLeft = true; faceDown = true;
 			faceRight = true; faceUp = true;
@@ -40,8 +39,7 @@ void Player::Update(float deltaTime)
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
 		if (faceRight == true) {
-			std::cout << "moving right" << std::endl;
-			movement.x += speed * deltaTime;
+			movement.x += ((speed * deltaTime) + 0.5);
 			direction = 4;
 			faceLeft = true; faceDown = true;
 			faceRight = true; faceUp = true;
@@ -49,8 +47,7 @@ void Player::Update(float deltaTime)
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
 		if (faceUp == true) {
-			std::cout << "moving up" << std::endl;
-			movement.y -= speed * deltaTime;
+			movement.y -= ((speed * deltaTime) + 0.5);
 			direction = 1;
 			faceLeft = true; faceDown = true;
 			faceRight = true; faceUp = true;
@@ -58,8 +55,7 @@ void Player::Update(float deltaTime)
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
 		if (faceDown == true) {
-			std::cout << "moving down" << std::endl;
-			movement.y += speed * deltaTime;
+			movement.y += ((speed * deltaTime) + 0.5);
 			direction = 2;
 			faceLeft = true; faceDown = true;
 			faceRight = true; faceUp = true;
@@ -67,59 +63,53 @@ void Player::Update(float deltaTime)
 	}
 	else {/*Player not moving*/ }
 
-/*
-	//sf::Vector2f movement(0.0f, 0.0f);
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-		//while (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {}
-		sound.play();
-		std::cout << "moving left" << std::endl;
-		movement.x -= speed * deltaTime;
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-		sound.play();
-		std::cout << "moving right" << std::endl;
-		movement.x += speed * deltaTime;
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-		sound.play();
-		std::cout << "moving up" << std::endl;
-		movement.y -= speed * deltaTime;
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-		sound.play();
-		std::cout << "moving down" << std::endl;
-		movement.y += speed * deltaTime;
-	}
-
-	*/
-
+	//Gerardo 4/13 made posxy global
 	//Switching map locations
-	const float playerposx = body.getPosition().x;
-	const float playerposy = body.getPosition().y;
-	std::cout << "x: " << playerposx << "     " << "y: " << playerposy << std::endl;
+	playerposx = body.getPosition().x;
+	playerposy = body.getPosition().y;
+	//std::cout << "x: " << playerposx << "     " << "y: " << playerposy << std::endl;
 
-	//checking if player is in east quadrant
-	if (body.getPosition().x > 720.0f) {
-		std::cout << "out of bounds East" << std::endl;
+	//East
+	if (body.getPosition().x > 720.0f && body.getPosition().y < 480.0f) {
 		view.setCenter(1080.0f, 240.0f);
-		//east = 1;
+		N = false, E = true, C = false, S = false, W = false, SW = false, SE = false;
+		here = 2;
 	}
-	//south quadrant
-	else if (body.getPosition().y > 480.0f) {
-		std::cout << "out of bounds South" << std::endl;
+	//South East
+	else if (body.getPosition().x > 720.0f && body.getPosition().y > 480.0f) {
+		view.setCenter(1080.0f, 720.0f);
+		N = false, E = false, C = false, S = false, W = false, SW = false, SE = true;
+		here = 7;
+	}
+	//South 
+	else if (body.getPosition().y > 480.0f && body.getPosition().x > 0) {
 		view.setCenter(360.0f, 720.0f);
+		N = false, E = false, C = false, S = true, W = false, SW = false, SE = false;
+		here = 3;
 	}
+	//South West
+	else if (body.getPosition().y > 480.0f && body.getPosition().x < 0) {
+		view.setCenter(-360.0f, 720.0f);
+		N = false, E = false, C = false, S = false, W = false, SW = true, SE = false;
+		here = 6;
+	}
+	//North
 	else if (body.getPosition().y < -50.0f) {
-		std::cout << "out of bounds North" << std::endl;
 		view.setCenter(360.0f, -240.0f);
+		N = true, E = false, C = false, S = false, W = false, SW = false, SE = false;
+		here = 1;
 	}
+	//West
 	else if (body.getPosition().x < -50.0f) {
-		std::cout << "out of bounds West" << std::endl;
 		view.setCenter(-360.0f, 240.0f);
+		N = false, E = false, C = false, S = false, W = true, SW = false, SE = false;
+		here = 4;
 	}
 	else {
 		view.setCenter(360.0f, 240.0f);
+		N = false, E = false, C = true, S = false, W = false, SW = false, SE = false;
+		here = 5;
 	}
 
 	if (movement.x == 0.0f)
@@ -130,17 +120,15 @@ void Player::Update(float deltaTime)
 	{
 		row = 1;
 		if (movement.x > 0.0f)
-			faceRight = true;
+			face = true;
 		else
-			faceRight = false;
+			face = false;
 	}
 	if (movement.y < 0.0f) {
 		row = 2;
-
 	}
-	
 
-	animation.Update(row, deltaTime, faceRight);
+	animation.Update(row, deltaTime, face);
 	body.setTextureRect(animation.uvRect);
 	body.move(movement);
 
