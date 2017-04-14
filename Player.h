3b2fbb4b1e42e3f1454d0d8c2f19ce2e4ca05f84
brianle192 <letancuong@gmail.com>
2017-04-14 00:01:57 -0700

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,8 +1,8 @@
+#pragma once
 #include <SFML\Graphics.hpp>
 #include <SFML\Audio.hpp>
 #include "Animation.h"
 #include <iostream>
-
 
 class Player
 {
@@ -29,59 +29,30 @@ public:
 	bool faceLeft = true;
 	bool faceUp = true;
 	bool faceDown = true;
-	
+
 	//Miguel
-	int maxHealth = 5;
+	double maxHealth = 5;
 	//Player Health+Ammo+Tower
-	int health = 5;
+	double health = 3;
+	int dead = 0;
 	int ammo = 3;
 	int tower = 3;
 	int gun = 0;
+	//Coordinates
+	bool N = false, E = false, C = false, S = false, W = false, SW = false, SE = false;
+	int here = 0;
+
+	//Gerardo Global Position of player
+	float playerposx = 0;
+	float playerposy = 0;
 
 
 private:
-	//sf::RectangleShape body;
-	//Animation animation;
-	//unsigned int row;
-	//float speed;
-	//bool faceRight;
+
 
 
 
 };
-
-
-
-/*
-class Player
-{
-public:
-	Player(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed);
-	~Player();
-
-	void Update(float deltaTime);
-	void Draw(sf::RenderWindow& window);
-
-	//Sound settings
-	sf::SoundBuffer soundBuf;
-	sf::Sound sound;
-	sf::View view;
-
-private:
-	sf::RectangleShape body;
-	Animation animation;
-	unsigned int row;
-	float speed;
-	bool faceRight;
-
-
-
-};
-
-*/
-
-
-
 
 
 
